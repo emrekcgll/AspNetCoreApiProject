c421a3c58184ac51fbf1824c7c39176db88b3a1f
Emre Koçoğlu <emrekcgll@gmail.com>
2023-06-21 16:52:34 +0300

    -

---
+++ JwtProject/WebApiJwt/Controllers/DefaultController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiJwt.Models;
+
+namespace WebApiJwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult CreateToken()
+        {
+            return Ok(new CreateToken().TokenCreate());
+        }
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult TestToken()
+        {
+            return Ok("Token onaylandı.");
+        }
+
+
+        [HttpGet("[action]")]
+        public IActionResult CreateAdminToken()
+        {
+            return Ok(new CreateToken().TokenCreateForAdmin());
+        }
+
+        [Authorize(Roles = "Admin, Visitor")]
+        [HttpGet("[action]")]
+        public IActionResult TestAdminToken()
+        {
+            return Ok("Admin token başarılı.");
+        }
+    }
+}
